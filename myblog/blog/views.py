eda4cb208e074f92db444297a011585dcf35fe57
programming <MariamBurjgi>
2024-03-20 18:07:24 +0400

    adding templates

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -21,8 +21,10 @@ def main(request):
   template = loader.get_template('main.html')
   return HttpResponse(template.render())
 def testing(request):
-  template = loader.get_template('template.html')
+  mymembers = Members.objects.all().values()
+  template =  loader.get_template('template.html')
   context = {
-    'fruits': ['Apple', 'Banana', 'Cherry'],   
+    'mymembers': mymembers,
   }
+  
   return HttpResponse(template.render(context, request))
